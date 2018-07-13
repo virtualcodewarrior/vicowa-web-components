7d91f3d7e6751239c46358fcf4f92c18d3191f98
Rodney Draaisma <Rodney.Draaisma@ni.com>
2018-07-13 14:15:53 -0400

    add panel with collapse feature

--- src/vicowa-panel/vicowa-panel.js
+++ src/vicowa-panel/vicowa-panel.js
@@ -1,7 +1,15 @@
 import { webComponentBaseClass } from '../third_party/web-component-base-class/dist/webComponentBaseClass.js';
 
 const componentName = 'vicowa-panel';
-window.customElements.define(componentName, class extends webComponentBaseClass {
+
+/**
+ * @extends webComponentBaseClass
+ * @property {string} header The header text
+ * @property {boolean} collapsible Indicates if the control is collapsible
+ * @property {boolean} expanded Indicates if the panel is expanded
+ * @property {string} expandControlAnimation Type of animation to use on the collapse control. Defaults to rotate
+ */
+class VicowaPanel extends webComponentBaseClass {
 	static get is() { return componentName; }
 	constructor() {
 		super();
@@ -15,6 +23,29 @@ window.customElements.define(componentName, class extends webComponentBaseClass 
 				reflectToAttribute: true,
 				observer: '_headerChanged',
 			},
+			collapsible: {
+				type: Boolean,
+				value: false,
+				reflectToAttribute: true,
+			},
+			expanded: {
+				type: Boolean,
+				value: true,
+				reflectToAttribute: true,
+			},
+			expandControlAnimation: {
+				type: String,
+				value: 'rotate',
+				reflectToAttribute: true,
+			},
 		};
 	}
-});
+
+	attached() {
+		this.addAutoEventListener(this.$.collapseControl, 'click', () => {
+			this.expanded = !this.expanded;
+		});
+	}
+}
+
+window.customElements.define(componentName, VicowaPanel);
