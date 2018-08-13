543ec9d936da0ce617e1fe08d66bd1c0725cfb7c
Rodney Draaisma <virtiualcodewarrior@hotmail.com>
2018-08-13 10:22:13 -0400

    More implementation for debug

--- src/utilities/debug.js
+++ src/utilities/debug.js
@@ -1,1 +1,9 @@
-export default window.console; // replace this with a dummy object in case of production
+/* eslint { 'no-console': 'off', 'no-empty-function': 'off' } */
+
+// this replaces console, use this instead of console in our code
+// this allows us to switch on and off actual console output
+// if window.develop is defined it will just copy the console functions, else it replaces all console commands with empty functions
+export default Object.keys(window.console).reduce((p_Previous, p_Key) => {
+	p_Previous[p_Key] = (window.develop) ? window.console[p_Key] : () => {};
+	return p_Previous;
+}, {});
