89506eede133a00c95a3066ea1e89974ed32877d
Rodney Draaisma <virtualcodewarrior@hotmail.com>
2020-03-21 19:30:02 -0400

    finish up notify component

--- src/vicowa-notify/vicowa-notify-box.js
+++ src/vicowa-notify/vicowa-notify-box.js
@@ -1,12 +1,22 @@
 import { webComponentBaseClass } from "../third_party/web-component-base-class/src/webComponentBaseClass.js";
-import { DEFAULT_LEVELS } from "src/vicowa-notify/vicowa-notify.js";
+
+export const DEFAULT_LEVELS = Object.freeze({
+	INFO: "INFO",
+	WARNING: "WARNING",
+	ERROR: "ERROR",
+});
 
 const privateData = Symbol("privateData");
 
 function closeBox(control) {
-	const controlData = control[privateData];
-
 	control.$.box.classList.add("closing");
+	clearTimeout(control[privateData].timeoutId);
+	control[privateData].timeoutId = undefined;
+}
+function setMessage(control) { control.$.message.string = control.message; }
+function setLevel(control) {
+	Object.values(DEFAULT_LEVELS).filter((level) => level !== control.level).forEach((level) => control.$.box.classList.remove(`level-${level.toLowerCase()}`));
+	control.$.box.classList.add(`level-${control.level.toLowerCase()}`);
 }
 
 const customElementName = "vicowa-notify-box";
@@ -15,86 +25,104 @@ window.customElements.define(customElementName, class extends webComponentBaseCl
 
 	constructor() {
 		super();
-
-		this[privateData] = {
-			timeoutId: undefined,
-		};
-		// extra required initialization goes here ...
+		this[privateData] = { timeoutId: undefined };
 	}
 
-	// add properties to this web component
 	static get properties() {
 		return {
-			message: {
-				type: String,
-				value: "",
-				reflectToAttribute: true,
-				notify: (control) => { control.$.message.innerHTML = control.message; },
-			},
-			level: {
-				type: String,
-				value: DEFAULT_LEVELS.INFO,
-				reflectToAttribute: true,
-			},
-			duration: {
-				type: Number,
-				value: 2000,
-				reflectToAttribute: true,
-			},
-			/*
-			 propertyName: { // the name of the property
-			 type: String, // (required) the type of the property, one of Array, Boolean, Number, Object, String
-			 value: 'value', // (optional) default value for the property
-			 reflectToAttribute: true, // (optional) indicate if you want the component attribute to always reflect the current property value
-			 observer: changeHandlerKey, // (optional) the name or a symbol for a function in the class to be called when the value of the property is changed
-			 },
-			 // add as many properties as you need ... */
+			message: { type: String, value: "", reflectToAttribute: true, notify: (control) => { setMessage(control); } },
+			level: { type: String, value: DEFAULT_LEVELS.INFO, reflectToAttribute: true, notify: (control) => { setLevel(control); } },
+			duration: { type: Number, value: 2000, reflectToAttribute: true },
 		};
 	}
 
-	// optional callback function that will be called after this instance of the web component
-	// has been added to the DOM
 	attached() {
-		this.$.message = this.message;
 		const controlData = this[privateData];
-		controlData.timeoutId = setTimeout(() => {
-			closeBox(this);
-		}, this.duration);
+		setMessage(this);
+		setLevel(this);
+		controlData.timeoutId = setTimeout(() => closeBox(this), this.duration);
 
 		this.addAutoEventListener(this.$.close, "click", () => closeBox(this));
+		this.addAutoEventListener(this.$.box, "transitionend", () => {
+			if (this.$.box.classList.contains("closing") && !this.$.box.classList.contains("collapsing")) {
+				this.$.box.classList.add("collapsing");
+			} else if (this.$.box.classList.contains("closing") && this.$.box.classList.contains("collapsing")) {
+				this.parentElement.removeChild(this);
+			}
+		});
 	}
 
-	// optional callback function that will be called after this instance of the web component has been removed from the DOM
-	detached() {
-		// extra cleanup that only can be done after an instance of the class has been removed from the DOM
-	}
+	detached() { closeBox(this); }
 
-	// string representation of the template to use with this web component
 	static get template() {
 		return `
             <style>
                 #box {
-                	transition: opacity 1000ms;
+                	box-sizing: border-box;
+                	margin: 0.3em 0;
+                	padding: 0.2em 1em;
+                	transition-property: opacity;
+                	transition-duration:  1000ms;
+                	max-height: var(--vicowa-notify-max-height, 30px);
+                	display: flex;
+                	align-content: space-between;
+                	border-radius: var(--vicowa-notify-border-radius, .3em);
+                	border-width: var(--vicowa-notify-border-width, 2px);
+                	border-style: var(--vicowa-notify-border-style: solid);
                 }
+                
+                #box > div {
+	                flex: 1 1 auto;
+	                padding: 2px 1em;
+	                display: flex;
+	                justify-content: center;
+	                align-items: center;
+                }
+                
                 #box.closing {
                 	opacity: 0;
                 }
+                #box.collapsing {
+                	transition-property: max-height, margin, border-width;
+                	transition-duration:  500ms;
+                	transition-timing-function: ease-out;
+                	max-height: 0;
+                	margin: 0;
+                	border-width: 0;
+                }
+                #close {
+                	background: var(--vicowa-notify-close-button-background, transparent);
+                	border: var(--vicowa-notify-close-button-border, none);
+                	font-weight: var(--vicowa-notify-close-button-font-weight, bold);
+                	pointer-events: all;
+                }
                 #box.level-info {
-                	background: black;
-                	color: white;
+                	background: var(--vicowa-notify-info-background, black);
+                	color: var(--vicowa-notify-info-color, white);
+                	border-color: var(--vicowa-notify-info-border-color, white);
+                }
+                #box.level-info #close {
+                	color: var(--vicowa-notify-info-close-button-color, white);
                 }
                 #box.level-warning {
-                	background: yellow;
+                	background: var(--vicowa-notify-warning-background, yellow);
+                	color: var(--vicowa-notify-warning-color, black);
+                	border-color: var(--vicowa-notify-warning-border-color, black);
+                }
+                #box.level-warning #close {
+                	color: var(--vicowa-notify-warning-close-button-color, black);
                 }
                 #box.level-error {
-                	background: red;
-                	color: white;
+                	background: var(--vicowa-notify-error-background, red);
+                	color: var(--vicowa-notify-error-color, white);
+                	border-color: var(--vicowa-notify-error-border-color, white);
+                }
+                #box.level-error #close {
+                	color: var(--vicowa-notify-error-close-button-color, white);
                 }
             </style>
             <div id="box">
-            	<slot id="content">
-            		<vicowa-string id="message"></vicowa-string><button id="close">x</button>
-				</slot>
+          		<div><vicowa-string id="message"></vicowa-string></div><button id="close">x</button>
 			</div>
         `;
 	}
--- src/vicowa-notify/vicowa-notify.js
+++ src/vicowa-notify/vicowa-notify.js
@@ -1,14 +1,7 @@
 import { webComponentBaseClass } from "../third_party/web-component-base-class/src/webComponentBaseClass.js";
 import "../vicowa-string/vicowa-string.js";
 import "../vicowa-button/vicowa-button.js";
-
-const privateData = Symbol("privateData");
-
-export const DEFAULT_LEVELS = Object.freeze({
-	INFO: "INFO",
-	WARNING: "WARNING",
-	ERROR: "ERROR",
-});
+import { DEFAULT_LEVELS } from "./vicowa-notify-box.js";
 
 export const DEFAULT_LOCATIONS = Object.freeze({
 	START: "START",
@@ -17,17 +10,38 @@ export const DEFAULT_LOCATIONS = Object.freeze({
 });
 
 function doMessage(control, properties, level) {
-	const controlData = control[privateData];
 	if (typeof properties === "string") {
 		properties = { message: properties };
 	}
 
-	properties = { ...{ level, locationX: control.LocationX, locationY: control.locationY, duration: control.duration }, ...properties };
+	properties = { ...{ level }, ...properties };
 
-	const message = {
+	const newNotification = document.createElement("vicowa-notify-box");
+	newNotification.message = properties.message;
+	newNotification.level = properties.level;
+	newNotification.duration = properties.duration;
 
-	};
-	controlData.notifications.push(message);
+	control.$.container.insertBefore(newNotification, control.$.container.firstChild);
+}
+
+function handleLocation(control) {
+	if (control.alignControl instanceof HTMLElement) {
+		const rect = control.alignControl.getBoundingClientRect();
+		control.$.container.style.left = `${rect.left}px`;
+		control.$.container.style.top = `${rect.top + rect.height}px`;
+		control.$.container.style.position = "absolute";
+	} else {
+		control.$.container.style.left = "";
+		control.$.container.style.top = "";
+		control.$.container.style.position = "relative";
+		if (!Object.values(DEFAULT_LOCATIONS).includes(control.locationX)) {
+			control.$.container.style.left = `${control.locationX}px`;
+			control.$.container.style.position = "absolute";
+		} if (!Object.values(DEFAULT_LOCATIONS).includes(control.locationY)) {
+			control.$.container.style.top = `${control.locationY}px`;
+			control.$.container.style.position = "absolute";
+		}
+	}
 }
 
 const customElementName = "vicowa-notify";
@@ -36,64 +50,54 @@ window.customElements.define(customElementName, class extends webComponentBaseCl
 
 	constructor() {
 		super();
-		// extra required initialization goes here ...
-
-		this[privateData] = {
-			notifications: [],
-		};
 	}
 
 	// add properties to this web component
 	static get properties() {
 		return {
-			duration: {
-				type: Number,
-				value: 5000,
-				reflectToAttribute: true,
-			},
-			locationX: {
-				type: String,
-				value: DEFAULT_LOCATIONS.CENTER,
-				refelectToAttribute: true,
-			},
-			locationY: {
-				type: String,
-				value: DEFAULT_LOCATIONS.END,
-				reflectToAttribute: true,
-			},
+			locationX: { type: String, value: DEFAULT_LOCATIONS.CENTER, reflectToAttribute: true, observer: handleLocation },
+			locationY: { type: String, value: DEFAULT_LOCATIONS.END, reflectToAttribute: true, observer: handleLocation },
+			alignControl: { type: Object, value: undefined, reflectToAttribute: false, observer: handleLocation },
 		};
 	}
 
-	info(properties) { doMessage(this, properties, DEFAULT_LEVELS.INFO); }
-	warning(properties) { doMessage(this, properties, DEFAULT_LEVELS.WARNING); }
-	error(properties) { doMessage(this, properties, DEFAULT_LEVELS.ERROR); }
-
-	// optional callback function that will be called after this instance of the web component
-	// has been added to the DOM
-	attached() {
-		// extra initialization that only can be done after an instance of the class has been attached to the DOM
-	}
-
-	// optional callback function that will be called after this instance of the web component has been removed from the DOM
-	detached() {
-		// extra cleanup that only can be done after an instance of the class has been removed from the DOM
-	}
+	info(properties) { doMessage(this, { duration: 2000, ...(typeof properties === "string" ? { message: properties } : properties) }, DEFAULT_LEVELS.INFO); }
+	warning(properties) { doMessage(this, { duration: 3500, ...(typeof properties === "string" ? { message: properties } : properties) }, DEFAULT_LEVELS.WARNING); }
+	error(properties) { doMessage(this, { duration: 5000, ...(typeof properties === "string" ? { message: properties } : properties) }, DEFAULT_LEVELS.ERROR); }
 
 	// string representation of the template to use with this web component
 	static get template() {
 		return `
             <style>
-                #container {
-                	display: block;
+                #main-box {
+                	display: flex;
                 	position: fixed;
+                	z-index: var(--vicowa-notify-z-index, 1000);
+                	left: 0;
+                	top: 0;
+                	bottom: 0;
+                	right: 0;
+                	padding: var(--vicowa-notify-padding, 1em);
+                	pointer-events: none;
+                }
+
+                #container {
+                	display: flex;
+                	flex-direction: column;
+                	align-items: center;
                 }
                 
+				:host([location-X="BEGIN"]) #main-box { justify-content: flex-start; }
+				:host([location-X="CENTER"]) #main-box { justify-content: center; }
+				:host([location-X="END"]) #main-box { justify-content: flex-end; }
+				:host([location-Y="BEGIN"]) #main-box { align-items: flex-start; }
+				:host([location-Y="CENTER"]) #main-box { align-items: center; }
+				:host([location-Y="END"]) #main-box { align-items: flex-end; }
+                
             </style>
-            <div id="container">
-            	<slot id="custom-message">
-            		<vicowa-string id="message"></vicowa-string>
-            	</slot>
-            	<vicowa-button id="close">X</vicowa-button>
+            <div id="main-box">
+            	<div id="container">
+            </div>
 			</div>
         `;
 	}
